9d7a2433e8c210c97a9d4e7f82cadf91415e7006
Wilson Lam <lin-1987@live.com>
2020-03-01 12:55:50 -0800

    Landing page completed

---
+++ assets/js/function.js
@@ -0,0 +1,21 @@
+// Landing page scroll functions
+let $window = $(window),
+    $body = $("body");
+
+// Guarantee that the page loads
+$window.on("load", () => {
+    $body.removeClass("preloaded");
+});
+
+$window.on("touchmove", () => {
+    return false;
+})
+
+$window.on("orientationchange", () => {
+    $body.scrollTop(0);
+})
+
+
+// window.onload = function() { document.body.classList.remove('is-preload'); }
+// window.ontouchmove = function() { return false; }
+// window.onorientationchange = function() { document.body.scrollTop = 0; }
